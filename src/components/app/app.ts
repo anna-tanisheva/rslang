db3cb9d3d883d7898f33029f9c903c94c13d8835
Anna Tanisheva <anna-tanisheva@yandex.ru>
2022-08-17 18:56:31 +0300

    feat: set up localstorage

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -4,7 +4,10 @@ import { AppView } from '../view/app-view';
 export class App {
   private view = new AppView();
 
+  private storage = localStorage;
+
   public start(): void {
     this.view.drawView();
+    console.log(this.storage);
   }
 }
