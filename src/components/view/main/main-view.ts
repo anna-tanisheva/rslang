84ad2af6f0b005f6c42aa71196eef3b07e0f0a8a
Anna Tanisheva <anna-tanisheva@yandex.ru>
2022-08-31 19:45:22 +0300

    feat: add charts to the statistic page

--- src/components/view/main/main-view.ts
+++ src/components/view/main/main-view.ts
@@ -6,6 +6,7 @@ import {ErrorView} from "./error-view";
 import { GamesPage } from "../games-start-page/games-page";
 import { StatisticView } from "../stats/statistic-view";
 import { appState } from "../../controller/state";
+// import { createCharts } from '../../controller/ui';
 
 export class MainView {
     public activeViewName: string | undefined;
@@ -25,6 +26,8 @@ export class MainView {
     create() {
         const main = createElementWithClassnames("main", "main");
         const overlay = createElementWithClassnames("div", "overlay", "hidden");
+
+
         if (
             this.activeViewName !== "index" &&
             this.activeViewName !== "textbook" &&
@@ -35,6 +38,7 @@ export class MainView {
             main.append(errorView);
             return main;
         }
+        // const chart = createCharts('audioCall');
         main.append(this[this.activeViewName], overlay);
         return main;
     }
--- src/components/view/stats/statistic-view.ts
+++ src/components/view/stats/statistic-view.ts
@@ -2,9 +2,12 @@ import './style.scss';
 import { IAppState, IUserStats } from "../../../typings";
 import { GameStatistic } from './game-statistic';
 import {
+  createElementWithAttributes,
   createElementWithClassnames,
   createElementWithContent,
 } from "../utils";
+import { setDailyChart } from '../../controller/ui';
+
 
 export class StatisticView {
 
@@ -14,11 +17,9 @@ export class StatisticView {
     let sprintStatistic;
     let callStatistic;
     let userStats;
-    let correctAnswersPercentContent;
     if(appState.isSignedIn) {
       userStats = appState.user.statsToday;
       wordsLearntContent = createElementWithContent('p', `Изучено слов: ${String((userStats as IUserStats).statisticState.total.wordsLearnt)}`);
-      correctAnswersPercentContent = createElementWithContent('p', `Вы ответили правильно на ${String((userStats as IUserStats).statisticState.total.correctAnswersPercent)} % вопросов`);
       correctAnswersContent = createElementWithContent('p', `Правильные ответы: ${String((userStats as IUserStats).statisticState.total.correctAnswers)}`);
       sprintStatistic = new GameStatistic((userStats as IUserStats).statisticState, 'sprint');
       callStatistic = new GameStatistic((userStats as IUserStats).statisticState, 'audioCall');
@@ -28,16 +29,25 @@ export class StatisticView {
       correctAnswersContent = createElementWithContent('p', `Правильные ответы: ${String(userStats.statisticState.total.correctAnswers)}`);
       sprintStatistic = new GameStatistic(userStats.statisticState, 'sprint');
       callStatistic = new GameStatistic(userStats.statisticState, 'audioCall');
-      correctAnswersPercentContent = createElementWithContent('p', `Вы ответили правильно на ${String((userStats as IUserStats).statisticState.total.correctAnswersPercent)} % вопросов`);
     }
     const statisticContainer = createElementWithClassnames("section", "statistic-container");
     const statisticHeader = createElementWithClassnames('h2', 'statistic-header');
     statisticHeader.textContent = 'Статистика за сегодня';
 
-    const statsToday = createElementWithClassnames('div', 'stats-today-wrapper');
-    statsToday.append(wordsLearntContent, correctAnswersContent, correctAnswersPercentContent);
-    statisticContainer.append(statisticHeader, statsToday, sprintStatistic.template, callStatistic.template);
+    const statisticHeaderToday = createElementWithClassnames('h2', 'statistic-header-total');
+    statisticHeaderToday.textContent = 'Общая статистика за сегодня';
 
+
+    const statsToday = createElementWithClassnames('div', 'stats-today-wrapper', 'statistic-wrapper');
+    const chartHTMLElemOptions = {
+      id: `total-chart`,
+      class: `chart`
+    }
+    const chart = createElementWithAttributes('canvas', chartHTMLElemOptions);
+    const data = [(userStats as IUserStats).statisticState.total.correctAnswersPercent, 100 - (userStats as IUserStats).statisticState.total.correctAnswersPercent];
+    setDailyChart((chart as HTMLCanvasElement), data)
+    statsToday.append(statisticHeaderToday, wordsLearntContent, correctAnswersContent, chart);
+    statisticContainer.append(statisticHeader, statsToday, sprintStatistic.template, callStatistic.template);
     return statisticContainer;
   }
 }
