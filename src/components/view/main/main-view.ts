40d5cea13bb5bea9093a3739ef16f7331531c24b
Anna Tanisheva <anna-tanisheva@yandex.ru>
2022-09-01 09:00:35 +0300

    merge commit from charts-feature

--- src/components/view/main/main-view.ts
+++ src/components/view/main/main-view.ts
@@ -3,16 +3,9 @@ import {MainPageView} from "../mainPage/mainPage-view";
 import {TextbookView} from "../textbook/textbook-view";
 import {createElementWithClassnames} from "../utils";
 import {ErrorView} from "./error-view";
-<<<<<<< HEAD
 import {GamesPage} from "../games-start-page/games-page";
 import {StatisticView} from "../stats/statistic-view";
 import {appState} from "../../controller/state";
-=======
-import { GamesPage } from "../games-start-page/games-page";
-import { StatisticView } from "../stats/statistic-view";
-import { appState } from "../../controller/state";
-// import { createCharts } from '../../controller/ui';
->>>>>>> charts-feature
 
 export class MainView {
     public activeViewName: string = appState.view;
