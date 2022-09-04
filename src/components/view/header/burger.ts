32e0fcb4dfaa803d1c5771342dd253591b31a577
badikgit <stas_yar@tut.by>
2022-09-04 22:24:50 +0300

    feat: add favicon

--- src/components/view/header/burger.ts
+++ src/components/view/header/burger.ts
@@ -1,15 +1,13 @@
 import { isHTMLElement } from "../../../typings/utils/utils";
 
 export function toggleMenu(e: Event): void {
+    if (!isHTMLElement(e.target)) return;
+    const navMenu = document.getElementById("nav-list");
+    if (!isHTMLElement(navMenu)) return;
+    const menu = document.getElementById("hamburger");
+    if (!isHTMLElement(menu)) return;
 
-  console.log(e.target);
-  if (!isHTMLElement(e.target)) return;
-  const navMenu = document.getElementById("nav-list");
-  if (!isHTMLElement(navMenu)) return;
-  const menu = document.getElementById("hamburger");
-  if (!isHTMLElement(menu)) return;
-
-  navMenu.classList.toggle("_active");
-  menu.classList.toggle("_active");
+    navMenu.classList.toggle("_active");
+    menu.classList.toggle("_active");
 }
 
